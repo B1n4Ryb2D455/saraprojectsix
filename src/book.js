7879a5c53322e4b9918efe3a28195ffda4f15b47
Sara Blevins <dvlnthebludrs@gmail.com>
2018-08-26 20:51:30 -0400

    Handle search errors

--- src/book.js
+++ src/book.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 class Book extends Component {
 	render() {
+		let shownThumbnail = this.props.book.imageLinks ?
+			this.props.book.imageLinks.thumbnail :
+			'';
+
 		return (
 			<div className="book">
 				<div className="book-top">
@@ -10,8 +14,7 @@ class Book extends Component {
 						style={{
 							width: 128,
 							height: 193,
-                            backgroundImage: `url("${this.props.book.imageLinks.thumbnail}"`
-                        }}>
+                            backgroundImage: `url("${shownThumbnail}"` }}>
                     </div>
 					<div className="book-shelf-changer">
                         <select
